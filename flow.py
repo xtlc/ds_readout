fb8c7ddb8b4003eb5d3a7e7c4da830e707cb24ce
rt <rt@gleebees.com>
2024-10-17 14:24:48 +0200

    nude flow first commit

---
+++ flow.py
@@ -0,0 +1,28 @@
+import time
+import RPi.GPIO as GPIO
+
+
+class Flow:
+    def __init__(self, FLOW_SENSOR_GPIO=13):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(FLOW_SENSOR_GPIO, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+        self.gpio
+        self.count = 0
+
+    def countPulse(self, channel):
+        if self.start_counter == 1:
+            self.count = self.count+1
+
+    def get_flow(self):
+        GPIO.add_event_detect(self.gpio, GPIO.FALLING, callback=self.countPulse)
+        while True:
+            self.start_counter = 1
+            time.sleep(1)
+            self.start_counter = 0
+            flow = (self.count/23)
+            print("The flow is: %.3f Liter/min" % (flow))
+            self.count = 0
+            time.sleep(5)
+
+
+print(Flow().get_flow())
