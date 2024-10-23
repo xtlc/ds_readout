59f09a460f709826f4b1ed5e533a425404f42271
rt <rt@gleebees.com>
2024-10-23 12:58:13 +0200

    formatting ...

--- do2.py
+++ do2.py
@@ -95,13 +95,14 @@ class Measurement:
             return f"""{formatted_value}"""
 
         if self.number_of_scales == 2:
-            head = "scale_left | scale_right | t_left_top | t_left_bot | t_mid_top | t_mid_bot | t_right_top | t_right_bot | h_left_top | h_left_bot | h_mid_top | h_mid_bot | h_right_top | h_right_bot | flow_left | flow_right "
+            head = "scale_left | scale_right | t_left_top | t_left_bot | t_mid_top | t_mid_bot | t_right_top | t_right_bot | h_left_top | h_left_bot | h_mid_top | h_mid_bot | h_right_top | h_right_bot | flow_left | flow_right |"
+            head+= "---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
             print(head)
             while True:
                 out = ""
                 if hasattr(self, "scales"): 
                     w = self.scales.get_all_weights()
-                    out += f"""{my_format(w["00"])} | {my_format(w["01"])} | """
+                    out += f"""{my_format(w["00"])} |  {my_format(w["01"])} | """
 
                 if hasattr(self, "temps"):
                     t = self.temps.get_all_temps()
