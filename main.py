b64fde71b3936beea33a2604d7fd2edf7116a880
rt <rt@gleebees.com>
2024-11-14 16:57:04 +0100

    added print to gui as new standard

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import flet as ft
 
+import sys
+import io
+import time
+
+class PrintCapture(io.StringIO):
+    def __init__(self, text_widget):
+        super().__init__()
+        self.text_widget = text_widget
+
+    def write(self, message):
+        super().write(message)
+        # Update the Flet text component with the new message
+        self.text_widget.value += message
+        self.text_widget.update()
+
+
+class MyClass:
+    def __init__(self):
+        self.do_something()
+
+    def do_something(self):
+        print("Doing something...")
+        time.sleep(2)
+        print("Something is done!")
 
 def main(page: ft.Page):
     headline = ft.Row(controls=[ft.Text(text_align=ft.TextAlign.CENTER, value=f"abaton measurement box", size=60,weight=ft.FontWeight.W_100,)],
@@ -9,18 +33,22 @@ def main(page: ft.Page):
 
     # Create a text element to display messages
     message = ft.Text("")
+    captured_text = PrintCapture(message)
+    sys.stdout = captured_text
 
     # Define button click handlers
     def calibrate_click(e):
+        message.value = "calibrating ..."
+        print("yoour mux")
         from scales import Mux
+        message.value="MUX imported"
         s = Mux(device="ttyUSB1", uid="0020240425142741", number_of_scales=2, max_values=0, sleep_time=0)
         s.zero_all_scales()
-        message.value = "calibrating ..."
         page.update()
 
     def test_click(e):
-        message.value = "testing..."
-        page.update()
+        print ("aldaaaeeer!")
+        #page.update()
 
     def log_click(e):
         message.value = "logging..."
