c4cedcb436dae965803dc0b078a113afeba26975
ra <rt@gleebees.com>
2024-12-04 19:48:17 +0100

    fixed a typo

--- pt100.py
+++ pt100.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 
 class PT100:
-    def __init__(self, PT100_WATER_IN_RIGHT, PT100_WATER_OUT_RIGHT, PT100_WATER_IN_LEFT, PT100_WATER_OUT_LEFT):
+    def __init__(self, PT100_WATER_IN_RIGHT=None, PT100_WATER_OUT_RIGHT=None, PT100_WATER_IN_LEFT=None, PT100_WATER_OUT_LEFT=None):
       #  self.pt100_in_r = f"""28-{PT100_WATER_IN_RIGHT}"""
       #  self.pt100_out_r = f"""28-{PT100_WATER_OUT_RIGHT}"""
       #  self.pt100_in_l = f"""28-{PT100_WATER_IN_LEFT}"""
@@ -14,23 +14,15 @@ class PT100:
       #  self.path = Path("/sys/bus/w1/devices/")
       #  self.sensors = [folder.name for folder in Path("/sys/bus/w1/devices/").glob("28-*/")] ## add /w1_slave
         self.sensors = {}
-        try:
-
+        if PT100_WATER_IN_RIGHT:
             self.sensors["in_ri"] =  Path("/sys/bus/w1/devices/").joinpath(f"""28-{PT100_WATER_IN_RIGHT}""", "w1_slave"),
-        except:
-            pass
-        try:
+        if PT100_WATER_OUT_RIGHT:
             self.sensors["out_ri"] = Path("/sys/bus/w1/devices/").joinpath(f"""28-{PT100_WATER_OUT_RIGHT}""", "w1_slave"),
-        except:
-            pass
-        try:
+        if PT100_WATER_IN_LEFT:
             self.sensors["in_le"] =  Path("/sys/bus/w1/devices/").joinpath(f"""28-{PT100_WATER_IN_LEFT}""", "w1_slave"),
-        except:
-            pass
-        try:
+        if PT100_WATER_OUT_LEFT:
             self.sensors["out_le"] = Path("/sys/bus/w1/devices/").joinpath(f"""28-{PT100_WATER_OUT_LEFT}""", "w1_slave")
-        except:
-            pass
+
 
     def get_temps(self):
         values = {}
@@ -50,7 +42,7 @@ class PT100:
 
 if __name__ == "__main__":
     print("Test mode running ...")    # You can call your function here if needed
-    f = PT100(PT100_WATER_IN_RIGHT="0000006a2c70", PT100_WATER_OUT_RIGHT="0000006ada1a", PT100_WATER_IN_LEFT="", PT100_WATER_OUT_LEFT="")
+    f = PT100(PT100_WATER_IN_RIGHT="0000006a2c70", PT100_WATER_OUT_RIGHT="0000006ada1a")
     while True:
         t = f.get_temps()
         print(t)
