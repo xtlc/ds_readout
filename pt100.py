ebfd0cf48a7dadc9c8577733ff09b97473aa4f9c
ra <keinepostnurmuell@gmail.com>
2024-12-05 09:15:56 +0100

    added help text

--- pt100.py
+++ pt100.py
@@ -34,7 +34,7 @@ class PT100:
 if __name__ == "__main__":
     print("Test mode running ...")    # You can call your function here if needed
     for item in BASEDIR.iterdir():
-        if item.is_dir():
+        if item.is_dir() and f"{item}".startswith("28-"):
             print(f"""found sensor: {item.name}""")
     f = PT100(PT100_WATER_IN_RIGHT="0000006a2c70", PT100_WATER_OUT_RIGHT="0000006ada1a")
     while True:
