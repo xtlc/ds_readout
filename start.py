58b5a4020ae8ff15784d2ef06341c5005a397cdd
ra <keinepostnurmuell@gmail.com>
2025-02-10 12:55:06 +0100

    tests added

--- start.py
+++ start.py
@@ -13,22 +13,30 @@ host = "https://eu-central-1-1.aws.cloud2.influxdata.com"
 host = "127.0.0.1:8186"
 bucket = env("BUCKET")
 
-mux_dict = {1: {"uid": "0120211005135155", "comment": "mux_4kg_1", "number_of_scales": 8}, 
-            2: {"uid": "0120211005135902", "comment": "mux_4kg_2", "number_of_scales": 8},
-            3: {"uid": "0020240425142741", "comment": "mux_8kg_1", "number_of_scales": 4},}  
+# mux_dict = {1: {"uid": "0120211005135155", "comment": "mux_4kg_1", "number_of_scales": 8}, 
+#             2: {"uid": "0120211005135902", "comment": "mux_4kg_2", "number_of_scales": 8},
+#             3: {"uid": "0020240425142741", "comment": "mux_8kg_1", "number_of_scales": 4},}  
+# 
+PT100s = {"in_ri": "0000006a2c70", "out_ri": "0000006ada1a", "in_le": "d5d3f91d64ff", "out_le": "a7d0f91d64ff"},
+USB_TEMP = "ttyUSB1"
+USB_SCALE = "ttyUSB0"
+FLOW_GPIOs = [12, 13]
+MUX = "0020240425142741"
+
+
 
 def zero_all_scales():
     print("Executing zero_all_scales()...")
     from scales import Mux
-    s = Mux(device="ttyUSB0", uid="0020240425142741", number_of_scales=2, max_values=0, sleep_time=0)
+    s = Mux(device=USB_SCALE, uid=MUX, number_of_scales=2, max_values=0, sleep_time=0)
     s.zero_all_scales()
 
 def to_terminal():
-    m = Measurement(device_temp_usb="ttyUSB1", 
-                    device_scale_usb="ttyUSB0", 
-                    pt100s={"in_ri": "0000006a2c70", "out_ri": "0000006ada1a", "in_le": "d5d3f91d64ff", "out_le": "a7d0f91d64ff"},
-                    scale_uid="0020240425142741", 
-                    device_flow_GPIOs=[12, 13], 
+    m = Measurement(device_temp_usb=USB_TEMP, 
+                    device_scale_usb=USB_SCALE, 
+                    pt100s=PT100s,
+                    scale_uid=MUX, 
+                    device_flow_GPIOs=FLOW_GPIOs, 
                     number_of_scales=2, 
                     measurements=0, 
                     sleep_time=3, 
@@ -42,12 +50,11 @@ def to_terminal():
     print("Executing to_terminal()...")
 
 def to_influx():
-    m = Measurement(device_temp_usb="ttyUSB1", 
-                    device_scale_usb="ttyUSB0", 
-                    pt100s={"in_ri": "0000006a2c70", "out_ri": "0000006ada1a", "in_le": "d5d3f91d64ff", "out_le": "a7d0f91d64ff"},
-                    #pt100s={"in_ri": "0000006a2c70", "out_ri": "0000006ada1a", "in_le": "0000006a2c70", "out_le": "a7d0f91d64ff"},
-                    scale_uid="0020240425142741", 
-                    device_flow_GPIOs=[12, 13], 
+    m = Measurement(device_temp_usb=USB_TEMP, 
+                    device_scale_usb=USB_SCALE, 
+                    pt100s=PT100s,
+                    scale_uid=MUX, 
+                    device_flow_GPIOs=FLOW_GPIOs, 
                     number_of_scales=2, 
                     measurements=0, 
                     sleep_time=60, 
@@ -79,6 +86,8 @@ def main():
         to_terminal()
     elif args.option == 2:
         to_influx()
+    elif args.option == 3:
+        test()
 
 if __name__ == "__main__":
     main()
