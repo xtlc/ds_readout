520fa86bbf445a098d7bcbe3928ea5dcb1df011f
ra <keinepostnurmuell@gmail.com>
2025-03-04 15:56:24 +0100

    sensor detection reworked

--- pt100.py
+++ pt100.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     for item in BASEDIR.iterdir():
         if item.is_dir():
             print(f"""found sensor: {item.name}""")
-            if item.name.startswith("00-"):
+            if item.name.startswith("00000-"):
                 sensors.append(item.name)
     print("------------------------------ T E S T I N G ----------------------------------------------")
     # f = PT100(PT100_WATER_IN_RIGHT="0000006a2c70", PT100_WATER_OUT_RIGHT="0000006ada1a", PT100_WATER_IN_LEFT="d5d3f91d64ff", PT100_WATER_OUT_LEFT="a7d0f91d64ff") ## for
@@ -56,7 +56,7 @@ if __name__ == "__main__":
                     print("1 x PT100 found")
                 except:
                     print("no PT100 found :(")
-
+                    exit()
 
     while True:
         t = f.get_temps()
